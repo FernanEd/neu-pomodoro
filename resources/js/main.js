a618a0836e15b9dbac11b23fc6c566168d777e78
fernaned <fernandoed.bm@gmail.com>
2021-05-17 15:51:54 -0500

    adding timin functions

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,45 @@
+const $btnNap = document.querySelector("#btnNap");
+const $btnWork = document.querySelector("#btnWork");
+const $btnTrip = document.querySelector("#btnTrip");
 
-window.myApp = {
-    showInfo: () => {
-        document.getElementById('info').innerHTML = NL_APPID + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + ".<br/><br/>" + "<span>v" + NL_VERSION + "</span>";
-    },
-    openDocs: () => {
-        Neutralino.app.open({
-            url: "https://neutralino.js.org/docs"
-        });
-    }
+const $timer = document.querySelector("#timer");
+
+$btnTrip.addEventListener("click", (e) => {
+  console.log("trip");
+});
+
+$btnWork.addEventListener("click", (e) => {
+  console.log("work");
+});
+
+$btnNap.addEventListener("click", (e) => {
+  console.log("nap");
+  runTimer(10);
+});
+
+const runTimer = (secondsLeft) => {
+  const stopTimer = async (timer) => {
+    clearInterval(timer);
+    await window.Neutralino.os.showMessageBox({
+      title: "STOP USING YOUR PC NOW!",
+      content: "Take a nap...",
+      type: "WARN",
+    });
+  };
+
+  let secondsGoneBy = 0;
+  $timer.innerText = formatTimer(secondsGoneBy);
+  let runningTimer = setInterval(() => {
+    secondsGoneBy++;
+    $timer.innerText = formatTimer(secondsGoneBy);
+    if (secondsGoneBy >= secondsLeft) stopTimer(runningTimer);
+  }, 1000);
 };
 
-Neutralino.init();
-window.myApp.showInfo();
+const formatTimer = (seconds) => {
+  minutes = seconds >= 60 ? (seconds / 60) | 0 : 0;
+  secondsleft = seconds >= 60 ? seconds % minutes : seconds;
+  return `${minutes < 10 ? "0" + minutes : minutes}:${
+    secondsleft < 10 ? "0" + secondsleft : secondsleft
+  }`;
+};
